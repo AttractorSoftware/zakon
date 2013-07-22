383dd61d22d9a9b5aa3d98340c505f632d3b3c32
daniel <afroprogger@gmail.com>
2013-07-22 10:46:05 +0600

    #10 сделал фильтр на загрузку добавил jquery в статик

--- django-project/document/forms.py
+++ django-project/document/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
+
 class UploadForm(forms.Form):
     doc_file = forms.FileField()
--- django-project/document/urls.py
+++ django-project/document/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.list, name='list'),
-    url(r'^upload/$', views.upload_file, name='upload'),
-    url(r'^(?P<doc_id>\d+)/$', views.law_detail, name='law')
+                       url(r'^$', views.list, name='list'),
+                       url(r'^upload/$', views.upload_file, name='upload'),
+                       url(r'^(?P<doc_id>\d+)/$', views.law_detail, name='law')
 )
