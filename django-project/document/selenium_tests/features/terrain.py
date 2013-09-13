e78beec7d6dadbcf76ae78a58f010a2211ada561
a.romashin <abd2561024@gmail.com>
2013-09-13 18:05:24 +0600

    #56  * Коммит для продолжения работы, т.к.  требуется ноутбук

--- django-project/document/selenium_tests/features/terrain.py
+++ django-project/document/selenium_tests/features/terrain.py
@@ -1,11 +1,16 @@
-
+from django.conf import settings
+from django.db import connection
 from lettuce import before, after, world
 from selenium import webdriver
-
+from django.core.management import call_command
 @before.all
 def initial_setup():
+    call_command('syncdb',interactive=False, verbosity=1)
     world.browser = webdriver.Firefox()
+    world.browser.implicitly_wait(10)
 
 @after.all
 def teardown_browser(total):
+    connection.creation.destroy_test_db(settings.DATABASES['default']['NAME'])
+    call_command('syncdb',interactive=False, verbosity=1)
     world.browser.quit()
