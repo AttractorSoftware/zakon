f641af12299728319827cb923ec73ee3731e30e0
galyuk <gra@mail.ru>
2013-11-22 11:13:42 +0600

    #95 Добавил чистку базы после каждого сценария в .feature

--- django-project/document/selenium_tests/features/terrain.py
+++ django-project/document/selenium_tests/features/terrain.py
@@ -5,9 +5,14 @@ from selenium import webdriver
 from django.core.management import call_command
 @before.all
 def initial_setup():
+    connection.creation.destroy_test_db(settings.DATABASES['default']['NAME'])
     call_command('syncdb',interactive=False, verbosity=1)
     world.browser = webdriver.Firefox()
     world.browser.implicitly_wait(10)
+
+@before.each_scenario
+def clean_up_db(scenario):
+    call_command('flush',interactive=False, verbosity=1)
 
 @after.all
 def teardown_browser(total):
