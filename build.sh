1b9c878c8361080503edbf61e36ac5d4fcd5a8f3
Ermek Kanybek uulu <kenjetaev.ermek@hotmail.com>
2013-11-29 12:28:56 +0600

    #100 вывод отчетности в консоль для автозагружаемых документов. игнорирование при выполнении приемочных тестов, сценария upload_all. Папка с документами передается через параметр степа

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd django-project
 pip install -r requirements-development.txt
-python manage.py harvest
+python manage.py harvest --tag=-skip_upload_all
 nosetests document/tests/
--- django-project/document/selenium_tests/features/upload_all.py
+++ django-project/document/selenium_tests/features/upload_all.py
@@ -2,37 +2,45 @@
 import os
 from os import walk
 from lettuce import *
-from zakon.settings import PROJECT_ROOT
 
-FILE_ROOT_ADDRESS = os.path.join(PROJECT_ROOT, 'document/selenium_tests/features/resources/')
-
+docsUploadedWithErrorCount = 0
 
 @step(u'я нахожусь на странице загрузки документа')
 def i_am_on_upload_page(step):
     world.browser.get("http://127.0.0.1:8000/upload/")
 
 
-@step(u'я загружаю все документы из каталога resources')
-def i_have_uploaded_document(step):
-    f = []
-    for (dirpath, dirnames, filenames) in walk(FILE_ROOT_ADDRESS):
-        f.extend(filenames)
+@step(u'я загрузил все документы из каталога "(.*?)"')
+def i_have_uploaded_document(step, docs_dir):
+    global docsUploadedWithErrorCount
+    filePaths = []
+    for (dirpath, dirnames, filenames) in walk(docs_dir):
+        filePaths.extend(filenames)
         break
-    for filePath in f:
-        filePath = FILE_ROOT_ADDRESS + filePath
+    for filePath in filePaths:
+        filePath = docs_dir + filePath
+        print u'загружаю документ "' + filePath + '"'
         i_am_on_upload_page(step)
         upload_document(step, filePath)
 
-@step(u'загружаю документ')
+    allDocsCount = str(len(filePaths))
+    docsUploadedWithoutErrorCount = str(len(filePaths) - docsUploadedWithErrorCount)
+    docsUploadedWithErrorCount = str(docsUploadedWithErrorCount)
+    print u"Количество всех документов: " + allDocsCount + "\r\n" + \
+          u"Количество загруженных без ошибок: " + docsUploadedWithErrorCount + "\r\n" + \
+          u"Количество загруженных с ошибками: " + docsUploadedWithoutErrorCount + "\r\n"
+
+
+@step(u'загружаю документ "(.*?)"')
 def upload_document(step, expected_file_name):
+    global docsUploadedWithErrorCount
     elem_browse_file = world.browser.find_element_by_id('id_doc_file')
     elem_browse_file.send_keys(expected_file_name)
     elem_upload = world.browser.find_element_by_id('btn_upload')
     elem_upload.click()
 
+    world.browser.implicitly_wait(0);
     try:
-        upload_anyway_button = world.browser.find_element_by_id('upload_anyway_button')
-    except Exception as e:
-        upload_anyway_button = 0
-    if upload_anyway_button != 0:
-        upload_anyway_button.click()
+        world.browser.find_element_by_id('upload_anyway_button').click()
+    except Exception:
+        docsUploadedWithErrorCount += 1
