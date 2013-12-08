17fb97fa9d3b6cbf24f3c52abeca603f2add1873
Dastan Kojomuratov <dastan.kozhomuratov@gmail.com>
2013-12-09 01:59:22 +0600

    #109 починил вывод результатов на jenkins * поставил плагины [http://wiki.jenkins-ci.org/display/JENKINS/Cobertura+Plugin cobertura] и [http://jenkins-server:8080/pluginManager/wiki.jenkins-ci.org/display/JENKINS/Violations violations] - эти плагины распарсивают результаты тестов * обновил скрипты запуска тестов * добавил новые зависимости типа django-discover-jenkins, для генерирования результатов в xml   * команда `manage.py tests --jenkins` создает отчеты тестов в папке reports

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd django-project
 pip install -r requirements-development.txt
-python manage.py harvest --tag=-skip_upload_all
-nosetests tests/
+python manage.py test -v 2 --jenkins
+python manage.py harvest --tag=-skip_upload_all --with-xunit --xunit-file=reports/lettuce.xml
--- django-project/zakon/settings_development.py
+++ django-project/zakon/settings_development.py
@@ -2,10 +2,19 @@ from settings import *
 
 INSTALLED_APPS = INSTALLED_APPS + (
     'tests.document',
-    'lettuce.django'
+    'lettuce.django',
+    'discover_jenkins',
 )
+
+TEST_RUNNER = 'discover_jenkins.runner.DiscoverCIRunner'
 
 LETTUCE_AVOID_APPS = (
     'document',
     'reference',
+    'discover_jenkins',
+
+)
+
+TEST_PROJECT_APPS = (
+    'tests',
 )
