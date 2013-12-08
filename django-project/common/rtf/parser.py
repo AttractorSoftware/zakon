2954072d5aca9a528edf0634cca22e04259983d8
n.koval <nikita.o.koval@outlook.com>
2013-12-08 14:45:52 +0600

    #99 * Вынес наборы контрольных слов и спец символов в отдельный файл. * Вынес ветвления в методы. * Вынес обработки в методы.

--- django-project/common/rtf/parser.py
+++ django-project/common/rtf/parser.py
@@ -1,156 +1,153 @@
 # -*- coding: UTF-8 -*-
 import struct
 import re
+from control_words import *
 from zakon.settings import RTF_DEFAULT_ENCODING
 
-SPECIAL_CHARS = {'par': '\n', 'sect': '\n\n', 'page': '\n\n', 'line': '\n', 'tab': '\t', 'emdash': u'\u2014',
-                 'endash': u'\u2013',
-                 'emspace': u'\u2003', 'enspace': u'\u2002', 'qmspace': u'\u2005', 'bullet': u'\u2022',
-                 'lquote': u'\u2018',
-                 'rquote': u'\u2019', 'ldblquote': u'\201C', 'rdblquote': u'\u201D', }
-
-RTF_CONTROL_WORDS = frozenset((
-    'aftncn', 'aftnsep', 'aftnsepc', 'annotation', 'atnauthor', 'atndate', 'atnicn', 'atnid', 'atnparent', 'atnref',
-    'atntime', 'atrfend', 'atrfstart', 'author', 'background', 'bkmkend', 'bkmkstart', 'blipuid', 'buptim', 'category',
-    'colorschememapping', 'colortbl', 'comment', 'company', 'creatim', 'datafield', 'datastore', 'defchp', 'defpap',
-    'do',
-    'doccomm', 'docvar', 'dptxbxtext', 'ebcend', 'ebcstart', 'factoidname', 'falt', 'fchars', 'ffdeftext', 'ffentrymcr',
-    'ffexitmcr', 'ffformat', 'ffhelptext', 'ffl', 'ffname', 'ffstattext', 'file', 'filetbl',
-    'fldtype', 'fname', 'fontemb', 'fontfile', 'fonttbl', 'footer', 'footerf', 'footerl', 'footerr', 'footnote',
-    'formfield', 'ftncn', 'ftnsep', 'ftnsepc', 'g', 'generator', 'gridtbl', 'header', 'headerf', 'headerl', 'headerr',
-    'hl',
-    'hlfr', 'hlinkbase', 'hlloc', 'hlsrc', 'hsv', 'htmltag', 'info', 'keycode', 'keywords', 'latentstyles', 'lchars',
-    'levelnumbers', 'leveltext', 'lfolevel', 'linkval', 'list', 'listlevel', 'listname', 'listoverride',
-    'listoverridetable', 'listpicture', 'liststylename', 'listtable', 'listtext', 'lsdlockedexcept', 'macc', 'maccPr',
-    'mailmerge', 'maln', 'malnScr', 'manager', 'margPr', 'mbar', 'mbarPr', 'mbaseJc', 'mbegChr', 'mborderBox',
-    'mborderBoxPr', 'mbox', 'mboxPr', 'mchr', 'mcount', 'mctrlPr', 'md', 'mdeg', 'mdegHide', 'mden', 'mdiff', 'mdPr',
-    'me',
-    'mendChr', 'meqArr', 'meqArrPr', 'mf', 'mfName', 'mfPr', 'mfunc', 'mfuncPr', 'mgroupChr', 'mgroupChrPr', 'mgrow',
-    'mhideBot', 'mhideLeft', 'mhideRight', 'mhideTop', 'mhtmltag', 'mlim', 'mlimloc', 'mlimlow', 'mlimlowPr', 'mlimupp',
-    'mlimuppPr', 'mm', 'mmaddfieldname', 'mmath', 'mmathPict', 'mmathPr', 'mmaxdist', 'mmc', 'mmcJc', 'mmconnectstr',
-    'mmconnectstrdata', 'mmcPr', 'mmcs', 'mmdatasource', 'mmheadersource', 'mmmailsubject', 'mmodso', 'mmodsofilter',
-    'mmodsofldmpdata', 'mmodsomappedname', 'mmodsoname', 'mmodsorecipdata', 'mmodsosort', 'mmodsosrc', 'mmodsotable',
-    'mmodsoudl', 'mmodsoudldata', 'mmodsouniquetag', 'mmPr', 'mmquery', 'mmr', 'mnary', 'mnaryPr', 'mnoBreak', 'mnum',
-    'mobjDist', 'moMath', 'moMathPara', 'moMathParaPr', 'mopEmu', 'mphant', 'mphantPr', 'mplcHide', 'mpos', 'mr',
-    'mrad',
-    'mradPr', 'mrPr', 'msepChr', 'mshow', 'mshp', 'msPre', 'msPrePr', 'msSub', 'msSubPr', 'msSubSup', 'msSubSupPr',
-    'msSup',
-    'msSupPr', 'mstrikeBLTR', 'mstrikeH', 'mstrikeTLBR', 'mstrikeV', 'msub', 'msubHide', 'msup', 'msupHide', 'mtransp',
-    'mtype', 'mvertJc', 'mvfmf', 'mvfml', 'mvtof', 'mvtol', 'mzeroAsc', 'mzeroDesc', 'mzeroWid', 'nesttableprops',
-    'nextfile', 'nonesttables', 'objalias', 'objclass', 'objdata', 'object', 'objname', 'objsect', 'objtime',
-    'oldcprops',
-    'oldpprops', 'oldsprops', 'oldtprops', 'oleclsid', 'operator', 'panose', 'password', 'passwordhash', 'pgp',
-    'pgptbl',
-    'picprop', 'pict', 'pn', 'pnseclvl', 'pntext', 'pntxta', 'pntxtb', 'printim', 'private', 'propname', 'protend',
-    'protstart', 'protusertbl', 'pxe', 'result', 'revtbl', 'revtim', 'rsidtbl', 'rxe', 'shp', 'shpgrp', 'shpinst',
-    'shppict', 'shprslt', 'shptxt', 'sn', 'sp', 'staticval', 'stylesheet', 'subject', 'sv', 'svb', 'tc', 'template',
-    'themedata', 'title', 'txe', 'ud', 'upr', 'userprops', 'wgrffmtfilter', 'windowcaption', 'writereservation',
-    'writereservhash', 'xe', 'xform', 'xmlattrname', 'xmlattrvalue', 'xmlclose', 'xmlname', 'xmlnstbl',
-    'xmlopen',     ))
-
-BOLD_STYLE_CONTROL_WORD = frozenset (('b'))
-PLAIN_STYLE_CONTROL_WORD = frozenset(('pard', 'plain'))
 
 class RTFParser(object):
     def __init__(self, encoding=RTF_DEFAULT_ENCODING):
         self._encoding = encoding
+        self.ignorable = False
+        self.is_plain = False
+        self.is_bold = False
+        self.uc_skip = 0        # Number of ASCII characters to skip after a unicode character.
+        self.cur_skip = 0       # Number of ASCII characters left to skip
+        self.stack = []         # Whether this group (and all inside it) are "ignorable".
+        self.bold_text = []
+        self.out = []
 
-    def parse(self, text):
-        pattern = re.compile(r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)", re.I)
+    def parse(self, rtf):
+        pattern = self.get_pattern_from_regex()
+        self.process_rtf_by_pattern(rtf, pattern)
+        return self.get_parsed_text()
 
-        stack = []
-        ignorable = False       # Whether this group (and all inside it) are "ignorable".
-        is_bold = False
-        bold_text = []
-        uc_skip = 0             # Number of ASCII characters to skip after a unicode character.
-        cur_skip = 0            # Number of ASCII characters left to skip
-        out = []                # Output buffer.
-        is_plain = False
-        for match in pattern.finditer(text):
+    def process_rtf_by_pattern(self, rtf, pattern):
+        for match in pattern.finditer(rtf):
             control_word, control_word_arg, hex, char, brace, tchar = match.groups()
+
             if brace:
-                cur_skip = 0
-                if brace == '{':
-                    stack.append((uc_skip, ignorable, is_bold))
-                elif brace == '}':
-                    uc_skip, ignorable, is_bold = stack.pop()
+                self.work_with_braces(brace)
             elif char:
-                cur_skip = 0
-                if char == '~':
-                    if not ignorable:
-                        out.append(u"\xA0")
-                elif char in '{}\\':
-                    if not ignorable:
-                        out.append(char)
-                        if is_bold:
-                            bold_text.append(char)
-                elif char == '*':
-                    ignorable = True
+                self.work_with_chars(char)
             elif control_word:
-                #cur_skip = 0
-                if control_word in BOLD_STYLE_CONTROL_WORD:
-                    is_bold = True
-                    is_plain = False
-                if control_word in PLAIN_STYLE_CONTROL_WORD:
-                    is_plain = True
-                if is_plain:
-                    is_bold = False
-                if control_word in RTF_CONTROL_WORDS:
-                    ignorable = True
-                elif ignorable:
-                    pass
-                elif control_word in SPECIAL_CHARS:
-                    if bold_text:
-                        bold_text.append(SPECIAL_CHARS[control_word])
-                    else:
-                        out.append(SPECIAL_CHARS[control_word])
-                elif control_word == 'uc':
-                    uc_skip = int(control_word_arg)
-                    cur_skip = uc_skip
-                elif control_word == 'u':
-                    if cur_skip > 0:
-                        cur_skip -= 1
-                    else:
-                        out.append(self.parse_unicode_char(control_word_arg))
-                    cur_skip = uc_skip
+                self.work_with_control_words_and_arguments(control_word, control_word_arg)
             elif hex:
-                if cur_skip > 0:
-                    cur_skip -= 1
-                elif not ignorable:
-                    if is_bold:
-                        bold_text.append(self.byte_to_unicode_char(hex))
-                    else:
-                        out.append(self.byte_to_unicode_char(hex))
+                self.work_with_hex(hex)
             elif tchar:
-                if cur_skip > 0:
-                    cur_skip -= 1
-                elif not ignorable:
-                    if is_bold:
-                        bold_text.append(tchar)
-                    else:
-                        out.append(tchar)
-            if not is_bold:
-                if not ignorable:
-                    out.extend(bold_text)
-                    if bold_text:
-                        position = len(out)
-                        if ''.join(bold_text).startswith(u"Статья"):
-                            out.insert(position, "\n")
-                    bold_text = []
-        return ''.join(out)
+                self.work_with_tchars(tchar)
+
+            self.append_and_modify_bold_text()
+
+    def get_parsed_text(self):
+        return ''.join(self.out)
+
+    def work_with_braces(self, brace):
+        self.cur_skip = 0
+        if brace == '{':
+            self.stack.append((self.uc_skip, self.ignorable, self.is_bold))
+        elif brace == '}':
+            self.uc_skip, self.ignorable, self.is_bold = self.stack.pop()
+
+    def work_with_chars(self, char):
+        self.cur_skip = 0
+        if char == '~':
+            if not self.ignorable:
+                space = u"\xA0"
+                self.append_symbol_to_array(space)
+        elif char in '{}\\':
+            if not self.ignorable:
+                self.append_symbol_to_array(char)
+        elif char == '*':
+            self.ignorable = True
+
+    def work_with_control_words_and_arguments(self, control_word, control_word_arg):
+        self.check_is_bold_control_word(control_word)
+        self.check_is_plain_text_control_word(control_word)
+        self.check_plain_text_begined()
+        self.check_is_rtf_control_word(control_word, control_word_arg)
+
+    def work_with_hex(self, hex):
+        if self.cur_skip > 0:
+            self.cur_skip -= 1
+        elif not self.ignorable:
+            symbol = self.byte_to_unicode_char(hex)
+            self.append_symbol_to_array(symbol)
+
+    def work_with_tchars(self, tchar):
+        if self.cur_skip > 0:
+            self.cur_skip -= 1
+        elif not self.ignorable:
+            self.append_symbol_to_array(tchar)
+
+    def check_is_bold_control_word(self, control_word):
+        if control_word in BOLD_STYLE_CONTROL_WORD:
+            self.is_bold = True
+            self.is_plain = False
+
+    def check_is_plain_text_control_word(self, control_word):
+        if control_word in PLAIN_STYLE_CONTROL_WORDS:
+            self.is_plain = True
+
+    def check_is_rtf_control_word(self, control_word, control_word_arg):
+        if control_word in RTF_CONTROL_WORDS:
+            self.ignorable = True
+        elif self.ignorable:
+            pass
+        elif control_word in SPECIAL_CHARS:
+            self.get_char_from_special_chars(control_word)
+        elif control_word == 'uc':
+            self.uc_skip = int(control_word_arg)
+            self.cur_skip = self.uc_skip
+        elif control_word == 'u':
+            code = control_word_arg
+            self.get_unicode_char_from_code(code)
+
+    def check_plain_text_begined(self):
+        if self.is_plain:
+            self.is_bold = False
+
+    def get_unicode_char_from_code(self, unicode_char):
+        if self.cur_skip > 0:
+            self.cur_skip -= 1
+        else:
+            self.out.append(self.parse_unicode_char_from_code(unicode_char))
+        self.cur_skip = self.uc_skip
+
+    def get_char_from_special_chars(self, control_word):
+        symbol = SPECIAL_CHARS[control_word]
+        self.append_symbol_to_array(symbol)
+
+    def append_and_modify_bold_text(self):
+        if not self.is_bold:
+            string_with_bold_text = ''.join(self.bold_text)
+            if string_with_bold_text.startswith(u"Статья"):
+                self.bold_text.append("\n")
+            self.out.extend(self.bold_text)
+            self.bold_text = []
+
+    def append_symbol_to_array(self, symbol):
+        if self.is_bold:
+            self.bold_text.append(symbol)
+        else:
+            self.out.append(symbol)
+
+    def get_pattern_from_regex(self):
+        pattern = re.compile(r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)", re.I)
+        return pattern
 
     def byte_to_unicode_char(self, hex):
-        c = int(hex, 16)
-        if c > 127:
-            return (struct.pack('B', c)).decode(self._encoding)
+        byte = int(hex, 16)
+        if byte > 127:
+            return (struct.pack('B', byte)).decode(self._encoding)
         else:
-            return chr(c)
+            return chr(byte)
 
-    def parse_unicode_char(self, control_word_arg):
-        c = int(control_word_arg)
-        if c < 0:
-            c += 0x10000
-        if c > 127:
-            return unichr(c)
+    def parse_unicode_char_from_code(self, code):
+        code = int(code)
+        if code < 0:
+            code += 0x10000
+        if code > 127:
+            return unichr(code)
         else:
-            return chr(c)
+            return chr(code)
